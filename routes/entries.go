061f54e719f1eac880b540f31b5e5209a6878a1a
Franso <francismuasya@gmail.com>
2022-07-18 12:07:38 +0300

    populate GetEntries function

--- routes/entries.go
+++ routes/entries.go
@@ -16,7 +16,29 @@ var entryCollection *mongo.Collection = openCollection(Client, "calories")
 
 func AddEntry(c *gin.Context) {}
 
-func GetEntries(c *gin.Context) {}
+func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entries []bson.M
+
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+
+}
 
 func GetEntriesByIngredient(c *gin.Context) {}
 
